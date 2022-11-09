35804059adb092904d3544be3a5012514e5e1713
Povladislav <vladpomalejko@gmail.com>
2022-11-09 20:50:23 +0300

    Updated categories and transact.

--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name_of_category = models.CharField(max_length=50, unique=True)
-    spent_sum = models.IntegerField(null=True)
+    spent_sum = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name_of_category
--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from categories.models import Category
 
 from .models import Transaction
 from .serializers import TransactionSerializer
@@ -12,3 +14,16 @@ class TransactionViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Transaction.objects.all()
     lookup_field = "id"
+
+    def create(self, request, *args, **kwargs):
+        category_pk = request.data['category']
+        category = Category.objects.get(pk=category_pk)
+        prev_sum = category.spent_sum
+        Transaction.objects.create(sum=request.data['sum'], category=category,
+                                   description=request.data.get('description', ''))
+        sum_to_add = prev_sum + int(request.data['sum'])
+        category.spent_sum = sum_to_add
+        category.save()
+        serializer = TransactionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
