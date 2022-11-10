eff96f073b2ae4affd358558ef2a40901b110974
Povladislav <vladpomalejko@gmail.com>
2022-11-10 14:52:31 +0300

    Added celery periodic tasks

--- counter/celery.py
+++ counter/celery.py
@@ -11,15 +11,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
-
-@app.task
-def test(arg):
-    print(arg)
-
-
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
--- counter/settings.py
+++ counter/settings.py
@@ -139,8 +139,8 @@ CELERY_TIMEZONE = 'Europe/Minsk'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'urflow43@gmail.com'
-EMAIL_HOST_PASSWORD = 'xkodkpgkhabapgen'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 
